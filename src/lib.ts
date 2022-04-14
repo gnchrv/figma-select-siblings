3d9a8a1559830b38e9248517cd04cc15067fc23c
Misha Goncharov <forsomeletters@gmail.com>
2022-04-15 01:26:21 +0200

    Make getNodeParents not to return DocumentNodes
    Previously, the `getNodeParents` function had been dealing with `SceneNode`s only, but had had the return type of `<BaseNode & ChildrenMixin>`. Consequently, it allowed the nodes returned from the function to be of the `DocumentNode` type.
    
    It had affected compilation process since you couldn’t treat the nodes returned from the function as the selectable ones (you can’t select `DocumentNode`’s children since they are pages).
    
    Because of that, after every function execution you’d had to explicitly check whether the returned nodes are of the `DocumentNode` type.
    
    It’s fixed now. `getNodeParents` returns `<(SceneNode | PageNode & ChildrenMixin)>`, so no additional validations required.

--- src/lib.ts
+++ src/lib.ts
@@ -1,18 +1,29 @@
 /**
  * Составляет набор родителей для списка элементов. Родители в списке не повторяются
  * @param nodes { readonly SceneNode[] } Список элементов, для каждого из которых нужно найти родителя
- * @returns {Set<BaseNode & ChildrenMixin>} Набор родителей
+ * @returns {Set<(SceneNode | PageNode) & ChildrenMixin>} Набор родителей
  */
-export function getNodesParents(nodes: readonly SceneNode[]): Set<BaseNode & ChildrenMixin> {
+export function getNodesParents(nodes: readonly SceneNode[]): Set<(SceneNode | PageNode) & ChildrenMixin> {
 
-    // Создаём заготовку, в которую будем добавлять найденных родителей
-    const parents = new Set<BaseNode & ChildrenMixin>()
+    // Создаём заготовку, в которую будем добавлять найденных родителей. Они будут являться элементами, располагающимися на холсте, или страницами. Также для компилятора нужно явно указать, что у них могут быть дети
+    const parents = new Set<(SceneNode | PageNode) & ChildrenMixin>()
 
     // Пробегаемся по каждому из элементов
     for (const node of nodes) {
+
+        /* 
+        Проверяем, не является ли родитель элемента целым документом. В этом случае сам элемент — страница и его нельзя выделить.
+    
+        На практике это невозможно, потому что функция получает на вход набор SceneNode — элементов, которые располагаются на холсте. Их родителями могут быть либо другие SceneNode, либо PageNode. Но никак не документ в целом.
         
-        // Находим родителя
-        const parent = node.parent
+        Однако пропустить эту проверку нельзя. По умолчанию родитель любого SceneNode имеет тип <BaseNode & ChildrenMixin>, а BaseNode, в свою очередь, включает в себя DocumentNode. Поэтому, если такую проверку не сделать, TS-компилятор будет ругаться.
+
+        Итого: если родитель выделенного элемента — целый документ, пропускаем его.
+    */
+        if (isDocumentNode(node.parent)) continue
+
+        // Выносим родителя в отдельную переменную
+        const parent: (SceneNode | PageNode) & ChildrenMixin = node.parent
 
         // Если такой родитель уже есть в регистре, переходим к следующему выделенному элементу
         if (parents.has(parent)) continue
